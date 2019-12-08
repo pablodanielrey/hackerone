55725fd966ef3f11912c4a643e3c486fe7c03a7c
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-12-08 05:19:53 -0300

    agregue para que itere sobre todos los bloques

--- encrypted-pastebin/test-padding-orgacle/test_encrypt_decrypt_last_block_binary.py
+++ encrypted-pastebin/test-padding-orgacle/test_encrypt_decrypt_last_block_binary.py
@@ -1,7 +1,7 @@
 from random import randrange
 from enc import encrypt, pad, decrypt, print_block, PaddingException
 
-def padding_oracle(c1:bytes,c2:bytes) -> bytes:
+def padding_oracle(c1:bytes, c2:bytes) -> bytes:
     w = memoryview(bytearray(16) + bytearray(c2))
     c1p = w[:16]
     des = [0] * 16 
@@ -17,26 +17,23 @@ def padding_oracle(c1:bytes,c2:bytes) -> bytes:
             c1p[i] = b
             try:
                 p_ = decrypt(bytes(w))
-
                 print_block(w)
                 print(f'datos desencriptados : {p_.hex()}')
                 print(f'encontrado : {b}')
                 print(f'bloque encontrado : {w.hex()}')
-
                 pad = 16 - i
                 des[i] = pad ^ c1[i] ^ c1p[i]
                 print(f'valor desencriptado : {des[i]:02x}')
-                
                 # ajusto el pad para el proximo byte a descubrir.
                 pad = pad + 1
                 for a in range(i,16):
                     c1p[a] = pad ^ des[a] ^ c1[a]
-
                 break
+
             except PaddingException:
                 pass
         else:
-            print('no se encontro nada')
+            print('no se encontro nada!!! esto es un error')
     return bytes(des)
 
 
@@ -50,11 +47,18 @@ hash_ = memoryview(h_)
 print(f'bloque a encriptar : {block_to_encrypt.hex()}')
 print(f'bloque encriptado  : {h_.hex()}')
 
-c1 = bytes(hash_[-32:-16])
-c2 = bytes(hash_[-16:])
-print(f'bloque a desencriptar {c2.hex()}')
+decrypted = None
 
-des = padding_oracle(c1,c2)
+for b in range(blocks):
+    i = b * 16
+    f = i + 16
+    c1 = hash_[i:f]
+    c2 = hash_[f:f+16]
+    des = padding_oracle(c1,c2)
+    if not decrypted:
+        decrypted = des
+    else:
+        decrypted = decrypted + des
 
-print(f'bloque original      : {block_to_encrypt[-16:].hex()}')
-print(f'bloque desencriptado : {des.hex()}')
+print(f'bloque original      : {block_to_encrypt[16:].hex()}')
+print(f'bloque desencriptado : {decrypted.hex()}')
